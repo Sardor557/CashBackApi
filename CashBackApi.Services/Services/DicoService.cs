e5650bb8b24561fc9c943418e309da2e1b387b9a
sardorahmedovt557 <sardorahmedovt557@gmail.com>
2023-07-20 11:34:55 +0500

    5451

---
+++ CashBackApi.Services/Services/DicoService.cs
@@ -0,0 +1,13 @@
+﻿using CashBackApi.Shared.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CashBackApi.Services.Services
+{
+    public sealed class DicoService : IDicoService
+    {
+    }
+}
